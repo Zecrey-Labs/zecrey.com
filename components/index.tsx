6c0fe444fc73fa1aeee279b9e60a14a5a079c1ac
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-05-06 11:11:19 +0800

    init section1

---
+++ components/index.tsx
@@ -0,0 +1,2 @@
+export { Header } from './header'
+export { Section1 } from './section1'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Header } from '@/components/header'
+import { Header, Section1 } from '@/components'
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Section1 />
     </div>
   )
 }
