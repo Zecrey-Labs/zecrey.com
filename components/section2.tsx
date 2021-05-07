9e8103f6f6c201fbcdb69062fa6cb1ac35be6fb4
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-05-08 07:28:04 +0800

    section 2

--- components/section2.tsx
+++ components/section2.tsx
@@ -8,34 +8,46 @@ import badge1 from '@/public/section2/badge1.svg'
 import badge2 from '@/public/section2/badge2.svg'
 import badge3 from '@/public/section2/badge3.svg'
 import badge4 from '@/public/section2/badge4.svg'
+import badge5 from '@/public/section2/badge5.svg'
 
 const Style = styled.div`
   height: 2000px;
   z-index: 9;
   position: relative;
-  div.keep {
-    position: relative;
-    width: 925px;
-    height: 75px;
+  padding: 273px 289px 179px 281px;
+  div.content {
     h2 {
-      border-radius: 5px;
-      background: #e4e4e4 0% 0% no-repeat padding-box;
+      width: 925px;
+      height: 87px;
       text-align: center;
       font: normal normal bold 36px/36px Lexend;
       letter-spacing: 0px;
       color: #383838;
       padding: 23px 0 16px 0;
-      z-index: 2;
-    }
-    img {
-      position: absolute;
-      &:nth-of-type(1) {
-        right: -18px;
-        top: 25px;
+      position: relative;
+      img {
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: -1;
       }
-      &:nth-of-type(2) {
-        right: -18px;
-        top: 51px;
+    }
+    div.text {
+      padding-left: 164px;
+      padding-top: 34px;
+      p {
+        text-align: left;
+        font: normal normal normal 24px/32px IBM Plex Sans;
+        letter-spacing: 0px;
+        color: #222222;
+        opacity: 1;
+        &:nth-of-type(1) {
+          width: 728px;
+          margin-bottom: 29px;
+        }
+        &:nth-of-type(2) {
+          width: 525px;
+        }
       }
     }
   }
@@ -44,35 +56,38 @@ const Style = styled.div`
 export const Section2 = () => {
   return (
     <Style>
-      <div className='keep'>
-        <h2>Keep Privacy in a Smart Contract World</h2>
-        <img src={badge1} alt='' />
-        <img src={badge2} alt='' />
+      <div className='content'>
+        <h2>
+          Keep Privacy in a Smart Contract World
+          <img src={badge5} alt='' />
+        </h2>
+        <div className='text'>
+          <p>
+            Problems of the privacy of blockchain right now are that used only
+            as privacy cash, in other words, it's non-programmable. In addition,
+            if the user wants to achieve asset privacy, it's not easy to use and
+            will cost a lot.
+          </p>
+          <p>
+            Zecrey is such a protocol that makes it easy to bring your assets
+            into a private world with high efficiency and low cost.
+          </p>
+        </div>
+        <ul className='features'>
+          <li>
+            <img src={icon1} alt='' />
+            <span>Public Scenes</span>
+          </li>
+          <li>
+            <img src={icon2} alt='' />
+            <span>Inefficient</span>
+          </li>
+          <li>
+            <img src={icon3} alt='' />
+            <span>High Cost</span>
+          </li>
+        </ul>
       </div>
-      <p>
-        Problems of the privacy of blockchain right now are that used only as
-        privacy cash, in other words, it's non-programmable. In addition, if the
-        user wants to achieve asset privacy, it's not easy to use and will cost
-        a lot.
-      </p>
-      <p>
-        Zecrey is such a protocol that makes it easy to bring your assets into a
-        private world with high efficiency and low cost.
-      </p>
-      <ul className='features'>
-        <li>
-          <img src={icon1} alt='' />
-          <span>Public Scenes</span>
-        </li>
-        <li>
-          <img src={icon2} alt='' />
-          <span>Inefficient</span>
-        </li>
-        <li>
-          <img src={icon3} alt='' />
-          <span>High Cost</span>
-        </li>
-      </ul>
     </Style>
   )
 }
