7310e6746ce3dfa3eb1a66aa9fc533b28860721b
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-05-07 22:56:32 +0800

    init section2

--- components/section2.tsx
+++ components/section2.tsx
@@ -4,19 +4,49 @@ import icon1 from '@/public/section2/icon1.svg'
 import icon2 from '@/public/section2/icon2.svg'
 import icon3 from '@/public/section2/icon3.svg'
 
+import badge1 from '@/public/section2/badge1.svg'
+import badge2 from '@/public/section2/badge2.svg'
+import badge3 from '@/public/section2/badge3.svg'
+import badge4 from '@/public/section2/badge4.svg'
+
 const Style = styled.div`
-  h2 {
-    background: #e4e4e4 0% 0% no-repeat padding-box;
-    border-radius: 5px;
+  height: 2000px;
+  div.keep {
+    position: relative;
     width: 925px;
     height: 75px;
+    h2 {
+      border-radius: 5px;
+      background: #e4e4e4 0% 0% no-repeat padding-box;
+      text-align: center;
+      font: normal normal bold 36px/36px Lexend;
+      letter-spacing: 0px;
+      color: #383838;
+      padding: 23px 0 16px 0;
+      z-index: 2;
+    }
+    img {
+      position: absolute;
+      &:nth-of-type(1) {
+        right: -18px;
+        top: 25px;
+      }
+      &:nth-of-type(2) {
+        right: -18px;
+        top: 51px;
+      }
+    }
   }
 `
 
 export const Section2 = () => {
   return (
     <Style>
-      <h2>Keep Privacy in a Smart Contract World</h2>
+      <div className='keep'>
+        <h2>Keep Privacy in a Smart Contract World</h2>
+        <img src={badge1} alt='' />
+        <img src={badge2} alt='' />
+      </div>
       <p>
         Problems of the privacy of blockchain right now are that used only as
         privacy cash, in other words, it's non-programmable. In addition, if the
