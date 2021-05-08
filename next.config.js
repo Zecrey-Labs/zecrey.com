1e4eb59a1b42fc363697b625a0347546401b66e9
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-05-08 23:12:53 +0800

    don't use asset prefix

--- next.config.js
+++ next.config.js
@@ -3,19 +3,29 @@ const withCSS = require('@zeit/next-css')
 const optimizedImages = require('next-optimized-images')
 const withImages = require('next-images')
 
-module.exports = withPlugins(
-  [
-    withCSS,
-    withImages,
-    [
-      optimizedImages,
-      {
-        inlineImageLimit: 8192
-      }
-    ]
-  ],
-  {
-    basePath: '/zecrey',
-    assetPrefix: '/zecrey/'
-  }
-)
+module.exports = withPlugins([
+  withCSS,
+  withImages({
+    webpack(config, options) {
+      return config
+    }
+  })
+  // [
+  //   optimizedImages,
+  //   {
+  //     mozjpeg: {
+  //       quality: 80
+  //     },
+  //     pngquant: {
+  //       speed: 3,
+  //       strip: true,
+  //       verbose: true
+  //     },
+  //     imagesPublicPath: '/zecrey/_next/static/images/'
+  //   }
+  // ],
+  // {
+  //   basePath: '/zecrey',
+  //   assetPrefix: '/zecrey/'
+  // }
+])
