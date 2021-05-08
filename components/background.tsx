3e2c7b22c08728c24f90e252efb90cbcdac3fa3b
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-05-08 08:36:06 +0800

    background 渐变

--- components/background.tsx
+++ components/background.tsx
@@ -5,63 +5,47 @@ import { useStore } from '@/store'
 const blue = require('@/public/section1/blue.png')
 const pink = require('@/public/section1/pink.png')
 
-const Style = styled.div`
-  &.dark {
-    div.dark {
-      display: normal;
-      @keyframes background-routine {
-        0% {
-          transform: translate(0);
-        }
-        50% {
-          transform: translateY(5px);
-        }
-        to {
-          transform: translate(0);
-        }
-      }
+const Style = styled.div<{ theme: 'dark' | 'light' }>`
+  z-index: -1;
+  min-width: 100vw;
+  min-height: 100vh;
+  position: fixed;
+  display: flex;
+  background-color: ${({ theme }) =>
+    theme === 'dark' ? 'rgba(34, 34, 34, 0.6)' : 'rgb(241, 241, 241)'};
 
-      z-index: -10;
-      min-width: 100vw;
-      min-height: 100vh;
-      position: fixed;
-      background-color: rgb(34, 34, 34);
-      display: flex;
-      div.black {
-        width: 100%;
-        min-height: 100vh;
-        background-color: rgba(34, 34, 34, 0.6);
-      }
-      img {
-        z-index: 1;
-        position: absolute;
-        animation: background-routine 3s linear infinite;
-      }
-      img:nth-of-type(1) {
-        top: 30px;
-        left: 7px;
-      }
-      img:nth-of-type(2) {
-        top: -481px;
-        left: -158px;
-      }
+  @keyframes background-routine {
+    0% {
+      transform: translate(0);
     }
-    div.light {
-      display: none;
+    50% {
+      transform: translateY(5px);
+    }
+    to {
+      transform: translate(0);
     }
   }
-  &.light {
-    div.dark {
-      display: none;
-    }
-    div.light {
-      display: normal;
-      z-index: 8;
-      min-width: 100vw;
-      min-height: 100vh;
-      position: fixed;
-      background-color: rgb(241, 241, 241);
-    }
+
+  div.dark {
+    z-index: -10;
+    flex: 1;
+    background-color: rgb(34, 34, 34);
+    opacity: ${({ theme }) => (theme === 'dark' ? 1 : 0)};
+    transition: opacity 0.6s ease-out;
+  }
+  img {
+    z-index: -1;
+    position: absolute;
+    animation: background-routine 3s linear infinite;
+    display: ${({ theme }) => (theme === 'dark' ? 'normal' : 'none')};
+  }
+  img:nth-of-type(1) {
+    top: 30px;
+    left: 7px;
+  }
+  img:nth-of-type(2) {
+    top: -481px;
+    left: -158px;
   }
 `
 
@@ -69,13 +53,10 @@ export const Background = observer(() => {
   const store = useStore()
 
   return (
-    <Style className={store.theme}>
-      <div className='dark'>
-        <div className='dark'></div>
-        <img src={blue} alt='' />
-        <img src={pink} alt='' />
-      </div>
-      <div className='light'></div>
+    <Style theme={store.theme}>
+      <div className='dark'></div>
+      <img src={blue} alt='' />
+      <img src={pink} alt='' />
     </Style>
   )
 })
