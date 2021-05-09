9236122e2e1749ffdf3c3584cfe12dbb831f4097
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-05-10 01:47:07 +0800

    section4 responsive design
    分1、2、4列展示

--- components/section4.tsx
+++ components/section4.tsx
@@ -17,41 +17,59 @@ const Style = styled.div`
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
-    padding: 203px 20px 0 20px;
+    padding-top: 203px;
 
-    @media (min-width: 1260px) and (max-width: 1650px) {
+    div.block {
+      padding: 10px;
+      div.text {
+        h3 {
+          text-align: left;
+          font: normal normal bold 38px/48px Lexend;
+          margin-bottom: 30px;
+          letter-spacing: 0px;
+          color: #e4e4e4;
+          text-shadow: 0px 3px 12px #00000040;
+          opacity: 1;
+        }
+        p {
+          text-align: left;
+          font: normal normal normal 24px/32px IBM Plex Sans;
+          letter-spacing: 0px;
+          color: #e4e4e4;
+          opacity: 1;
+        }
+      }
+    }
+
+    @media (max-width: 860px) {
+      div.block {
+        display: flex;
+        flex-direction: row;
+        padding: 30px;
+        div.image {
+          flex: 0 0 130px;
+        }
+      }
+    }
+    /* 两列 */
+    @media (min-width: 860px) and (max-width: 1650px) {
       width: 760px;
       box-sizing: content-box;
     }
-
-    div.block {
-      width: 360px;
-      box-sizing: content-box;
-      padding: 10px;
-      div.image {
-        height: 100px;
-        position: relative;
-        img {
-          position: absolute;
-          left: 0;
-          bottom: 0;
+    /* 两列、四列 */
+    @media (min-width: 860px) {
+      div.block {
+        box-sizing: content-box;
+        width: 360px;
+        div.image {
+          height: 100px;
+          position: relative;
+          img {
+            position: absolute;
+            left: 0;
+            bottom: 0;
+          }
         }
-      }
-      h3 {
-        text-align: left;
-        font: normal normal bold 38px/48px Lexend;
-        margin-bottom: 30px;
-        letter-spacing: 0px;
-        color: #e4e4e4;
-        text-shadow: 0px 3px 12px #00000040;
-        opacity: 1;
-      }
-      p {
-        text-align: left;
-        font: normal normal normal 24px/32px IBM Plex Sans;
-        letter-spacing: 0px;
-        color: #e4e4e4;
-        opacity: 1;
       }
     }
   }
@@ -94,8 +112,10 @@ export const Section4 = observer(() => {
             <div className='image'>
               <Img src={block.image} />
             </div>
-            <h3>{block.title}</h3>
-            <p>{block.text}</p>
+            <div className='text'>
+              <h3>{block.title}</h3>
+              <p>{block.text}</p>
+            </div>
           </div>
         ))}
       </div>
