a1cd8defee6eca71a828b48af65b33b8ffef9d5d
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-05-10 22:01:11 +0800

    section1 responsive

--- components/section1.tsx
+++ components/section1.tsx
@@ -7,17 +7,8 @@ import { validateEmail } from '@/utils'
 
 const InputEmail = styled.input`
   display: block;
-  width: 455px;
-  @media (max-width: 455px) {
-    width: 100%;
-  }
-  height: 52px;
   background: #222222 0% 0% no-repeat padding-box;
-  border: 2px solid #2ad4d9;
-  border-radius: 5px;
-  padding: 10px 20px;
   text-align: left;
-  font: normal normal normal 24px/32px IBM Plex Sans;
   letter-spacing: 0px;
   color: #e4e4e4;
   &:focus {
@@ -31,14 +22,10 @@ const InputEmail = styled.input`
 
 const Button = styled.button`
   background: #2ad4d9 0% 0% no-repeat padding-box;
-  border-radius: 5px;
   opacity: 1;
   text-align: left;
-  font: normal normal bold 24px/29px Lexend;
   letter-spacing: 0px;
   color: #222222;
-  width: 120px;
-  height: 52px;
   text-align: center;
   border: none;
 `
@@ -53,9 +40,9 @@ const Style = styled.div`
   div.message {
     position: fixed;
     width: 100%;
-    top: 110px;
+    top: max(1.1rem, 64px);
     z-index: 100;
-    font: bold 36px/70px Lexend;
+    font: bold 0.36rem/0.7rem Lexend;
     text-align: center;
     box-shadow: 0 0.3rem 1.2rem rgb(0 0 0 / 25%);
     color: #383838;
@@ -64,50 +51,64 @@ const Style = styled.div`
   h2 {
     margin-top: 1.28rem;
     background: #2ad4d9 0% 0% no-repeat padding-box;
-    height: 50px;
-    width: calc(100% - 20px);
-    max-width: 383px;
-    font: normal normal bold 32px/50px Lexend;
-    border-radius: 5px;
+    height: 0.5rem;
+    width: 3.83rem;
+    font: normal normal bold 0.32rem/0.5rem Lexend;
+    border-radius: 0.05rem;
     text-align: center;
     letter-spacing: 0px;
     color: #222222;
     position: relative;
     img {
-      font-size: 10px;
+      font-size: 0.1rem;
       position: absolute;
-      right: 23px;
-      top: -5px;
+      right: 0.23rem;
+      top: 0;
+      transform: translateY(-50%);
+    }
+    @media (max-width: 1279px) {
+      height: 33px;
+      width: 255px;
+      font: normal normal bold 21px/33px Lexend;
+      border-radius: 3px;
+      img {
+        right: 15px;
+      }
     }
   }
   div.banner {
-    margin-top: 25px;
+    margin-top: 0.25rem;
     text-align: center;
     letter-spacing: 0px;
     color: #e4e4e4;
-    text-shadow: 0px 3px 12px #00000040;
+    text-shadow: 0px 0.03rem 0.12rem #00000040;
     text-transform: uppercase;
-    @media (max-width: 999px) {
-      font: normal normal bold 60px/80px Lexend;
-    }
-    @media (min-width: 1000px) {
-      font: normal normal bold 1rem/1.2rem Lexend;
+    font: normal normal bold 1rem/1.2rem Lexend;
+    @media (max-width: 1279px) {
+      font: normal normal bold 66px/80px Lexend;
     }
   }
   div.email {
-    margin-top: 69px;
+    margin-top: 0.69rem;
     position: relative;
     display: flex;
     align-items: center;
     input {
       transition: transform 0.2s ease-out;
-      z-index: 2;
+      z-index: 1;
+      box-sizing: border-box;
+      width: 4.55rem;
+      height: 0.52rem;
+      border: 0.02rem solid #2ad4d9;
+      border-radius: 0.05rem;
+      padding-left: 0.2rem;
+      font: normal normal normal 0.24rem/0.32rem IBM Plex Sans;
     }
     img {
       position: absolute;
       bottom: 0;
-      right: 5px;
-      height: 26px;
+      right: 0.05rem;
+      height: 0.26rem;
       z-index: 0;
       transition: transform 0.2s ease-out;
     }
@@ -117,22 +118,26 @@ const Style = styled.div`
       right: 0;
       transition: transform 0.2s ease-out;
       z-index: 0;
+      font: normal normal bold 0.24rem/0.29rem Lexend;
+      width: 1.2rem;
+      height: 0.52rem;
+      border-radius: 0.05rem;
     }
     &:hover {
       input {
-        transform: translateX(-20px);
+        transform: translateX(-0.2rem);
         cursor: pointer;
       }
       img {
-        transform: translateX(19px);
+        transform: translateX(0.19rem);
       }
       button {
-        transform: translateX(-20px);
+        transform: translateX(-0.2rem);
       }
     }
     &.active {
       input {
-        transform: translateX(-20px);
+        transform: translateX(-0.2rem);
       }
       img {
         visibility: hidden;
@@ -143,8 +148,30 @@ const Style = styled.div`
     }
   }
   > img {
-    height: 52px;
-    margin: 141px 0 0px 0;
+    height: 0.52rem;
+    margin-top: 1.41rem;
+  }
+  @media (max-width: 1279px) {
+    div.email {
+      input {
+        width: 303px;
+        height: 35px;
+        border: 1px solid #2ad4d9;
+        border-radius: 3px;
+        padding-left: 13.3px;
+        font: normal normal normal 16px/21px IBM Plex Sans;
+      }
+      button {
+        font: normal normal bold 16px/19px Lexend;
+        width: 80px;
+        height: 34px;
+        border-radius: 3px;
+      }
+    }
+    > img {
+      height: 35px;
+      margin-top: 94px;
+    }
   }
 `
 
