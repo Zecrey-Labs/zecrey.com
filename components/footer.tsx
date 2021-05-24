89b2da4dc9c17ae19af3e130877597a8c10a9795
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-05-24 19:01:37 +0800

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -21,11 +21,14 @@ const Style = styled.div`
     top: 1.63rem;
   }
   > div.content {
+    &.mobile {
+      display: none;
+    }
     max-width: 15.19rem;
     margin: 0 auto;
     padding-top: 2.96rem;
     position: relative;
-    div.row1 {
+    div.section1 {
       z-index: 1;
       border-bottom: 1px solid rgb(228, 228, 228);
       display: flex;
@@ -38,7 +41,7 @@ const Style = styled.div`
         position: relative;
         top: -0.32rem;
       }
-      > div.column {
+      > div.subsection {
         margin-right: 1.03rem;
         display: flex;
         flex-direction: column;
@@ -51,14 +54,14 @@ const Style = styled.div`
         }
       }
     }
-    div.row2 {
+    div.section2 {
       display: flex;
       flex-direction: row;
       justify-content: space-between;
       background-color: rgb(34, 34, 34);
       align-items: center;
       padding: 0.3rem 0;
-      div.column1 {
+      div.subsection1 {
         display: flex;
         align-items: center;
         font-size: 0.3rem;
@@ -68,25 +71,73 @@ const Style = styled.div`
           margin: 0 0.17rem;
         }
       }
-      div.column2 {
+      div.subsection2 {
         text-align: right;
         font: normal normal normal 0.2rem/0.27rem Leelawadee UI;
         color: #e4e4e4;
       }
     }
   }
-  @media (max-width: 500px) {
+  @media (max-width: 760px) {
     div.content {
+      &.desktop {
+        display: none;
+      }
+      &.mobile {
+        display: block;
+      }
+      div.row1 {
+        background-color: rgb(34, 34, 34);
+        > div {
+          margin: 0 36px;
+          display: flex;
+          flex-direction: row;
+          align-items: flex-end;
+          justify-content: space-between;
+          padding-bottom: 12px;
+          border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+          div.left {
+            width: 97px;
+            img {
+              width: 100%;
+              height: auto;
+              margin-bottom: 16px;
+            }
+            ul.nav {
+              li {
+                font-family: Lexend;
+                font-style: normal;
+                font-weight: 800;
+                font-size: 12px;
+                line-height: 15px;
+                color: #00adb1;
+                margin-bottom: 10px;
+                &:last-of-type {
+                  margin-bottom: 2px;
+                }
+              }
+            }
+          }
+          div.right {
+            display: flex;
+            flex-direction: row;
+            font-size: 16px;
+            svg {
+              font-size: 0.38rem;
+              color: #00acb1;
+              margin-left: 20px;
+            }
+          }
+        }
+      }
       div.row2 {
-        flex-direction: column;
-        div.column1 {
-          justify-content: center;
-          margin-bottom: 0.3rem;
-        }
-        div.column2 {
-          text-align: center;
-          font: normal normal normal 14px/16px Leelawadee UI;
-        }
+        font-family: Lexend;
+        text-align: center;
+        font-size: 9px;
+        line-height: 11px;
+        color: rgba(255, 255, 255, 0.6);
+        padding: 9px 36px 40px 36px;
+        background-color: rgb(34, 34, 34);
       }
     }
   }
@@ -98,28 +149,52 @@ export const Footer = observer(() => {
   return (
     <Style style={{ opacity: store.theme === 'dark' ? 1 : 0 }}>
       <Img className='wave' src='/footer/wave.svg' />
-      <div className='content'>
-        <div className='row1'>
+      <div className='content desktop'>
+        <div className='section1'>
           <Img className='icon' src={'/logo1.svg'} alt='' />
-          <div className='column'>
+          <div className='subsection'>
             <span>White paper</span>
             <span>App</span>
           </div>
-          <div className='column'>
+          <div className='subsection'>
             <span>Team</span>
             <span>About</span>
           </div>
         </div>
-        <div className='row2'>
-          <div className='column1'>
+        <div className='section2'>
+          <div className='subsection1'>
             <FontAwesomeIcon icon={['fab', 'twitter']} />
             <FontAwesomeIcon icon={['fab', 'instagram']} />
             <FontAwesomeIcon icon={['fab', 'github']} />
             <FontAwesomeIcon icon={['fab', 'discord']} />
           </div>
-          <div className='column2'>
+          <div className='subsection2'>
             Copyright © 2021 Zecrey. All rights reserved.
           </div>
+        </div>
+      </div>
+      <div className='content mobile'>
+        <div className='row1'>
+          <div>
+            <div className='left'>
+              <Img className='icon' src={'/logo1.svg'} alt='' />
+              <ul className='nav'>
+                <li>White paper</li>
+                <li>App</li>
+                <li>Team</li>
+                <li>About</li>
+              </ul>
+            </div>
+            <div className='right'>
+              <FontAwesomeIcon icon={['fab', 'twitter']} />
+              <FontAwesomeIcon icon={['fab', 'instagram']} />
+              <FontAwesomeIcon icon={['fab', 'github']} />
+              <FontAwesomeIcon icon={['fab', 'discord']} />
+            </div>
+          </div>
+        </div>
+        <div className='row2'>
+          Copyright © 2021 Zecrey. All rights reserved.
         </div>
       </div>
     </Style>
