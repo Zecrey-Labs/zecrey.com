b3e61636020020dd70cad6df20e14c76d32b2ffe
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-08-30 10:28:44 +0800

    update download url

--- constant/index.ts
+++ constant/index.ts
@@ -10,5 +10,5 @@ export const META = {
 }
 
 export const DOWNLOAD_URL =
-  'https://zecrey.obs.ap-southeast-3.myhuaweicloud.com:443/Zecrey%20Testnet%20Chrome%20Extension.zip?AccessKeyId=BEEB9AHVC2WCDCOIOETF&Expires=1632796069&Signature=%2BZA3M9BT6IjY6TXJ97mjU35HeXE%3D'
+  'https://zecrey.obs.ap-southeast-3.myhuaweicloud.com/Zecrey%20Testnet%20Chrome%20Extension.zip'
 export const DOCS_URL = 'https://docs.zecrey.com/'
