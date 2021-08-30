a8c5e48ad9cbfca6bf4cb73396a6fd96bcaa09a0
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-08-30 20:32:05 +0800

    init new section6

---
+++ components/Section6.tsx
@@ -0,0 +1,14 @@
+/*!
+ * Copyright (C) 2020-present, Zecrey-Labs
+ */
+
+import React from 'react'
+import styled from 'styled-components'
+
+const Style = styled.div``
+
+export const Section6 = () => {
+  return <Style>
+
+  </Style>
+}
