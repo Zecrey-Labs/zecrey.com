0cf3ef75af19d690ba45e63a2b061ec641ec1d1b
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-09-01 15:04:06 +0800

    [footer] add email

--- components/Footer/style.ts
+++ components/Footer/style.ts
@@ -2,17 +2,10 @@
  * Copyright (C) 2020-present, Zecrey-Labs
  */
 
-import React, { useState } from 'react'
 import styled from 'styled-components'
-import Img from './img'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(fab)
-import { px2vw, validateEmail } from '@/utils'
-import { DOCS_URL, DOWNLOAD_URL } from '@/constant'
+import { desktopPx2vw, px2vw } from '@/utils'
 
-const Style = styled.div`
+export const Style = styled.div`
   position: relative;
   width: 100vw;
   z-index: 1;
@@ -88,11 +81,43 @@ const Style = styled.div`
           margin: 0 ${px2vw(17, 1920)};
         }
       }
-      div.subsection2 {
+      > div.subsection2 {
         text-align: right;
         font: normal normal normal ${px2vw(20, 1920)} / ${px2vw(27, 1920)}
           Leelawadee UI;
         color: #e4e4e4;
+        > div.contact {
+          display: inline-block;
+          em {
+            font-family: Lexend;
+            font-style: normal;
+            font-weight: 600;
+            font-size: 24px;
+            line-height: 30px;
+            color: #e4e4e4;
+            margin-right: ${desktopPx2vw(6)};
+          }
+          span {
+            font-family: sans-serif;
+            font-size: 21px;
+            line-height: 25px;
+            text-decoration-line: underline;
+            color: #e4e4e4;
+          }
+        }
+        > span.bar {
+          width: ${desktopPx2vw(1)};
+          height: ${desktopPx2vw(20)};
+          margin: 0 ${desktopPx2vw(21)};
+          background-color: #e4e4e4;
+        }
+        > div.copyright {
+          display: inline-block;
+          font-family: sans-serif;
+          font-size: 21px;
+          line-height: 25px;
+          color: #e4e4e4;
+        }
       }
     }
   }
@@ -171,7 +196,7 @@ const Style = styled.div`
     }
   }
 `
-const Message = styled.div`
+export const Message = styled.div`
   position: fixed;
   width: 100%;
   top: max(${px2vw(110, 1920)}, 64px);
@@ -185,7 +210,7 @@ const Message = styled.div`
     top: 40px;
   }
 `
-const EmailSubscribe = styled.div<{ ac: boolean }>`
+export const EmailSubscribe = styled.div<{ ac: boolean }>`
   position: absolute;
   bottom: ${px2vw(32, 1920)};
   display: flex;
@@ -244,156 +269,3 @@ const EmailSubscribe = styled.div<{ ac: boolean }>`
     }
   }
 `
-
-export const Footer = () => {
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = React.useState(null)
-
-  React.useEffect(() => {
-    let timeoutID
-    if (message) {
-      timeoutID = setTimeout(() => {
-        setMessage(null)
-      }, 1000)
-    }
-    return () => {
-      timeoutID && clearTimeout(timeoutID)
-    }
-  }, [message])
-
-  const subscribe = async () => {
-    if (validateEmail(email)) {
-      try {
-        const response = await fetch('/api/subscribe?address=' + email)
-        const result = await response.json()
-        if (result.message === 'subscribed') setEmail('')
-        setMessage(
-          result.message === 'subscribed'
-            ? 'Subscribe Successfully'
-            : result.error
-        )
-      } catch (error) {
-        setMessage('Failed to subscribe')
-      }
-    } else {
-      setMessage('Invalid Email')
-    }
-  }
-
-  return (
-    <Style>
-      <Img className='wave' src='/footer/wave.svg' />
-      <div className='content desktop'>
-        <div className='section1'>
-          <Img className='icon' src={'/logo1.svg'} alt='' />
-          <div className='subsection'>
-            <a href='https://docsend.com/view/ntcsmt7meu84gcqk' target='_blank'>
-              White paper
-            </a>
-            <a
-              href='https://zecrey.obs.ap-southeast-3.myhuaweicloud.com:443/Zecrey%20Testnet%20Chrome%20Extension.zip?AccessKeyId=BEEB9AHVC2WCDCOIOETF&Expires=1632673207&Signature=xP%2BvTHtSvIv8SbIgA6udJ0y4zug%3D'
-              className='downloadapp'
-              download='zecrey'>
-              App
-            </a>
-            <a href='https://docs.zecrey.com/' target='_blank'>
-              Document
-            </a>
-          </div>
-          <div className='email-subscribe'>
-            <EmailSubscribe ac={email ? true : false}>
-              <label>Subscribe:</label>
-              <input
-                type='text'
-                placeholder='Send us your email address'
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-              />
-              <button onClick={subscribe}>Okay</button>
-            </EmailSubscribe>
-          </div>
-        </div>
-        <div className='section2'>
-          <div className='subsection1'>
-            <a href='https://twitter.com/zecreyprotocol' target='_blank'>
-              <FontAwesomeIcon icon={['fab', 'twitter']} />
-            </a>
-            <a href='https://medium.com/@zecrey' target='_blank'>
-              <FontAwesomeIcon icon={['fab', 'medium']} />
-            </a>
-            <a href='https://github.com/Zecrey-Labs' target='_blank'>
-              <FontAwesomeIcon icon={['fab', 'github']} />
-            </a>
-            <a href='https://t.me/zecrey' target='_blank'>
-              <FontAwesomeIcon icon={['fab', 'telegram']} />
-            </a>
-            <a href='https://discord.com/invite/U98ghQsJE5' target='_blank'>
-              <FontAwesomeIcon icon={['fab', 'discord']} />
-            </a>
-          </div>
-          <div className='subsection2'>
-            Copyright © 2021 Zecrey. All rights reserved.
-          </div>
-        </div>
-      </div>
-      <div className='content mobile'>
-        <div className='row1'>
-          <div>
-            <div className='left'>
-              <Img className='icon' src={'/logo1.svg'} alt='' />
-              <div className='nav'>
-                <a
-                  href='https://docsend.com/view/ntcsmt7meu84gcqk'
-                  target='_blank'>
-                  White paper
-                </a>
-                <a
-                  href={DOWNLOAD_URL}
-                  className='downloadapp'
-                  download='zecrey'>
-                  App
-                </a>
-                <a href={DOCS_URL} target='_blank'>
-                  Document
-                </a>
-              </div>
-            </div>
-            <div className='right'>
-              <a href='https://twitter.com/zecreyprotocol' target='_blank'>
-                <FontAwesomeIcon icon={['fab', 'twitter']} />
-              </a>
-              <a href='https://medium.com/@zecrey' target='_blank'>
-                <FontAwesomeIcon icon={['fab', 'medium']} />
-              </a>
-              <a href='https://github.com/Zecrey-Labs' target='_blank'>
-                <FontAwesomeIcon icon={['fab', 'github']} />
-              </a>
-              <a href='https://t.me/zecrey' target='_blank'>
-                <FontAwesomeIcon icon={['fab', 'telegram']} />
-              </a>
-              <a href='https://discord.com/invite/U98ghQsJE5' target='_blank'>
-                <FontAwesomeIcon icon={['fab', 'discord']} />
-              </a>
-            </div>
-          </div>
-          <div className='email-subscribe'>
-            <EmailSubscribe ac={email ? true : false}>
-              <label>Subscribe:</label>
-              <input
-                type='text'
-                placeholder='email address'
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-              />
-              <button onClick={subscribe}>Okay</button>
-            </EmailSubscribe>
-          </div>
-        </div>
-        <div className='row2'>
-          Copyright © 2021 Zecrey. All rights reserved.
-        </div>
-      </div>
-      {message && <Message className='message'>{message}</Message>}
-    </Style>
-  )
-}
