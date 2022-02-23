ac38608bd5c6f7fd0aea7d16b95d140740f9f939
shaopeili <97080920+shaopeili@users.noreply.github.com>
2022-02-23 08:57:54 +0800

    Multifunctional components and drop-down menus

---
+++ pages/infopulldown.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pulldown from '/components/infopulldown/Pulldown'
+
+function infopulldown() {
+  return (
+    <div>
+      <Pulldown/>
+    </div>
+  )
+}
+
+export default infopulldown
