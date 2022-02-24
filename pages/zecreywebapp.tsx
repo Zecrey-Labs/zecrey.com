491c612bf58b72bde59cadab1a8c1b4be7508c26
shaopeili <97080920+shaopeili@users.noreply.github.com>
2022-02-24 10:41:41 +0800

    extension wallet sum zecrey web app sum zecrey mobile

---
+++ pages/zecreywebapp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ZecreyWebApp from '@/components/Zecreywebapp/ZecreyWebApp'
+
+function zecreywebapp() {
+  return (
+    <div>
+     <ZecreyWebApp/>
+    </div>
+  )
+}
+
+export default zecreywebapp
