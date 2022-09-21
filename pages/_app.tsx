0efdc563b1d72c7bc3b411e09517efdab42453f0
DH <7332456+amoslover@users.noreply.github.com>
2022-09-21 14:02:07 +0800

    Update index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    window.history.scrollRestoration = "manual";
+  }, []);
+
   return (
     <>
       <Head>
