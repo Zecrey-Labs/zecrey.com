c2676ea698d62e1ad0f36d4490308b08dc21320d
DH <7332456+amoslover@users.noreply.github.com>
2022-10-27 14:25:05 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,28 @@
 import Communtity from "components/community";
 import Extension from "components/extension";
-import Landing from "components/landing";
 import Layout from "components/layout";
-import MobileApp from "components/mobile";
-import MultiChains from "components/multi-chains";
-import MultiPurpose from "components/multi-purpose";
-import Backers from "components/backers";
-import RoadMap from "components/roadmap";
-import WebApp from "components/web-app";
-import Layer2 from "components/layer2";
-import Legend from "components/legend";
-import Zero from "components/zero";
-import Scenarios from "components/application-scenarios";
-import Ecosystem from "components/multi-ecosystem";
+import dynamic from "next/dynamic";
+
+const Landing = dynamic(() => import("components/landing"), { ssr: false });
+const MobileApp = dynamic(() => import("components/mobile"), { ssr: false });
+const MultiChains = dynamic(() => import("components/multi-chains"), {
+  ssr: false,
+});
+const MultiPurpose = dynamic(() => import("components/multi-purpose"), {
+  ssr: false,
+});
+const Backers = dynamic(() => import("components/backers"), { ssr: false });
+const RoadMap = dynamic(() => import("components/roadmap"), { ssr: false });
+const WebApp = dynamic(() => import("components/web-app"), { ssr: false });
+const Layer2 = dynamic(() => import("components/layer2"), { ssr: false });
+const Legend = dynamic(() => import("components/legend"), { ssr: false });
+const Zero = dynamic(() => import("components/zero"), { ssr: false });
+const Scenarios = dynamic(() => import("components/application-scenarios"), {
+  ssr: false,
+});
+const Ecosystem = dynamic(() => import("components/multi-ecosystem"), {
+  ssr: false,
+});
 
 const Home = () => {
   return (
