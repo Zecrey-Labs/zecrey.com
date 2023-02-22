6462978201d442f805a4d3838128a2f295da87f2
wangyafei <wyfblues@163.com>
2023-02-22 15:18:02 +0800

    Optimize the first screen loading logic

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import Header from "@/components/layout/header";
-import Footer from "@/components/layout/footer";
 import Screen1 from "@/components/screen/screen1";
+import Footer from "@/components/layout/footer";
 
 export default function RootLayout (props: { children: ReactNode | undefined }) {
 
